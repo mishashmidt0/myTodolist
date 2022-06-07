fcf08a7a5f18a41fcfcfb1dbbb0dac60bddcd424
mishashmidt <mishashmidt0@gmail.com>
2022-06-07 23:00:16 +0300

    feat: add api and stories task

--- src/stories/todolist-api.stories.tsx
+++ src/stories/todolist-api.stories.tsx
@@ -1,86 +1,84 @@
 import React, {useEffect, useState} from "react";
-import axios from "axios";
+import todoAPI from "../api/todolists-api";
+
 
 export default {
     title: "Api test"
 }
 
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true,
-    headers: {
-        "API-KEY": "918ec3c9-ea92-470f-ae86-9d6bbf63b653"
-    }
-})
-
 
 export const getTodolist = () => {
-    const [todo, setTodo] = useState<any>(null)
 
+    const [arrTodo, setArrTodo] = useState<any>(null)
 
     useEffect(() => {
-        instance.get("todo-lists").then(res => {
-                setTodo(res.data)
-            }
-        )
-
+        todoAPI.getTodo(setArrTodo)
     }, [])
 
-    return <div> {JSON.stringify(todo)} </div>
+    return <div> {arrTodo && arrTodo.map((el: any, index: number) => {
+        return (
+            <div key={el.id}>
+                <h2>Task №{index}</h2>
+                <h3>Title: {el.title}</h3>
+                <span>ID: {el.id}</span>
+                <hr/>
+            </div>
+        )
+    })} </div>
 }
 
 export const postTodolist = () => {
     const [todo, setTodo] = useState<any>(null)
+    const [text, setText] = useState<any>(null)
 
-    useEffect(() => {
-        const title = "React tasks"
-        instance.post("todo-lists", {title: title}).then(res => {
 
-                setTodo(res.data.data.item)
-            }
-        )
+    const creatPost = () => {
+        todoAPI.postTodo(text, setTodo)
+    }
 
-    }, [])
-
-    return <div> {JSON.stringify(todo)} </div>
+    return <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+        <button onClick={creatPost}>add</button>
+        <hr/>
+        {JSON.stringify(todo)} </div>
 }
 
 export const deleteTodolistAll = () => {
     const [todo, setTodo] = useState<any>(null)
-
-
     useEffect(() => {
-        instance.get("todo-lists").then(res => {
-
-                res.data.forEach((item: any) => {
-                    instance.delete(`todo-lists/${item.id}`).then(res => {
-
-                            setTodo(res.data)
-                        }
-                    )
-                })
-
-            }
-        )
+        todoAPI.deleteAllTodo(setTodo)
 
     }, [])
-
-
     return <div> {JSON.stringify(todo)} </div>
 }
 
 export const deleteTodolist = () => {
     const [todo, setTodo] = useState<any>(null)
+    const [text, setText] = useState<string>('')
 
-    useEffect(() => {
-        const todolistId = "123"
-        instance.delete(`todo-lists/id${todolistId}`)
-            .then(res => setTodo(res))
-            .catch(err => {
-                setTodo(err.response.data.message)
-            })
+    const deleteTodo = () => {
+        todoAPI.deleteTodolistById(text, setTodo)
 
-    }, [])
+    }
+    return <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)}/>
+        <button onClick={deleteTodo}>delete</button>
+        {JSON.stringify(todo)}
+    </div>
+}
 
-    return <div> {JSON.stringify(todo)} </div>
+export const updateTodolist = () => {
+    const [todo, setTodo] = useState<any>(null)
+    const [text, setText] = useState<any>(null)
+    const [id, setId] = useState<any>(null)
+
+    const update = () => {
+        todoAPI.updateTodo(id, text, setTodo)
+    }
+    return <div>
+        <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder={'new text'}/>
+        <input type="text" value={id} onChange={(e) => setId(e.target.value)} placeholder={'title id'}/>
+        <button onClick={update}>update</button>
+        <hr/>
+        {JSON.stringify(todo)} </div>
 }
